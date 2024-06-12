eae6a519b2c1d1f198d584d9a92218edc3858f24
Kolovratik <Kolovratik@HOME-PC>
2024-06-12 20:03:26 +0300

    Проработанная структура и реализованное главное окно

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PR44
 {
@@ -20,9 +7,12 @@ namespace PR44
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            DataContext = new VM_Pages
         }
     }
 }
