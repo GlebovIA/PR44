15b599025a089a0022825495bf93192f730e5ae1
Kolovratik <Kolovratik@HOME-PC>
2024-06-12 21:17:54 +0300

    Разработанная модель представления главного окна

---
+++ ViewModels/VM_Pages.cs
@@ -0,0 +1,20 @@
+﻿using PR44.Classes;
+
+namespace PR44.ViewModels
+{
+    public class VM_Pages : Notification
+    {
+        public VM_Tasks vm_tasks = new VM_Tasks();
+        public VM_Pages()
+        {
+            MainWindow.init.frame.Navigate(new View.Main(vm_tasks));
+        }
+        public RelayCommand OnClose
+        {
+            get
+            {
+                return new RelayCommand(obj => MainWindow.init.Close());
+            }
+        }
+    }
+}
