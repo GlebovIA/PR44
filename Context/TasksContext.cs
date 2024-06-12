16c9b300209ddb5a506ded2d52b0310ca9f9cf8b
Kolovratik <Kolovratik@HOME-PC>
2024-06-12 21:14:26 +0300

    Разработанный контекст данных для модели задач

---
+++ Context/TasksContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PR44.Classes.DataBase;
+using PR44.Models;
+
+namespace PR44.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(Config.connection, Config.version);
+        }
+    }
+}
