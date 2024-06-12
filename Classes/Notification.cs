a8fe6dbfd57f780644e7ab8fc82b962b3dfeeb92
Kolovratik <Kolovratik@HOME-PC>
2024-06-12 20:44:34 +0300

    Разработанный классы общего-пользования

---
+++ Classes/Notification.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PR44.Classes
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
