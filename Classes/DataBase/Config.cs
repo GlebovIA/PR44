8b22b5d8040182856191e0b747f968daf1d484b5
Kolovratik <Kolovratik@HOME-PC>
2024-06-12 20:35:49 +0300

    Созданный класс Config

---
+++ Classes/DataBase/Config.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace PR44.Classes.DataBase
+{
+    public class Config
+    {
+        public static readonly string connection = "server=;" +
+            "uid=root;" +
+            "pwd=;" +
+            "database=TaskManager;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
